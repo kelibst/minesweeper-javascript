4828bbd24ec25b6069869cd2118d4b94cc082311
Keli Booster <kbooster17@gmail.com>
2021-02-18 05:29:56 +0000

    reconfigure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,52 @@
-// Webpack uses this to work with directories
-const path = require('path');
+const Htmlwebpackplugin = require('html-webpack-plugin');
+const Minicss = require('mini-css-extract-plugin');
 
-// This is the main configuration object.
-// Here, you write different options and tell Webpack what to do
 module.exports = {
-
-  // Path to your entry point. From this file Webpack will begin its work
-  entry: './src/javascript/index.js',
-
-  // Path and filename of your result bundle.
-  // Webpack will bundle all JavaScript into this file
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '',
-    filename: 'bundle.js'
+  module: {
+    rules: [
+      {
+        test: /\.(scss|sass|css)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            esModule: false,
+          },
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
+      },
+    ],
   },
-
-  // Default mode for Webpack is production.
-  // Depending on mode Webpack will apply different things
-  // on the final bundle. For now, we don't need production's JavaScript 
-  // minifying and other things, so let's set mode to development
-  mode: 'development'
+  plugins: [
+    new Htmlwebpackplugin({
+      template: '/src/index.html',
+      filename: './index.html',
+    }),
+    new Minicss({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
 };
