8e9b2f0ee3188255dc0855d106c720c195548b4e
Keli Booster <kbooster17@gmail.com>
2021-02-22 20:21:12 +0000

    style page

--- src/javascript/checkWin.js
+++ src/javascript/checkWin.js
@@ -2,7 +2,6 @@ const checkWin = (squares, matches, bombAmount) => {
     for (let i = 0; i < squares.length; i++) {
         if (squares[i].classList.contains('flag') && squares[i].classList.contains('bomb')) {
             matches ++
-            console.log('another mathces', matches)
         } 
         if (matches === bombAmount) {
             console.log('you won')
